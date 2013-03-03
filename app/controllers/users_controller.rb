f07d944ac6de0bb2bd1d62615051518d9d97267d
ZhaoZijie <hot.shikong@gmail.com>
2013-03-03 11:39:03 +0800

    x

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
+#encoding: utf-8
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = '欢迎来到四川理工订餐网'
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 end
